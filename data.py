c6478e39fb6dbd1c4ca804f99eb28e91fe1207f0
Shehani Wetthasinghe <sheha919@gmail.com>
2022-08-29 17:21:33 -0400

    add data generating code

--- data.py
+++ data.py
@@ -16,21 +16,23 @@ def der_energy(i,j,file_name):
                        der=x
                        print('function1: '+i+' '+der)
                        value[0]=y
-                       print('function2 '+str(value[0])) 
+                       print('function2 '+str(value[0]))
                elif x== j:
                        der=x
                        print('function1: '+j+' '+der)
                        value[1]=y
                        print('function2 '+str(value[1]))
-       
+
         file_x.close()
         return float(value[0]),float(value[1])
 
 
 d=np.empty(4)
-der_data = pd.DataFrame(columns=['Name', 'CoCp2 energy', 'CoCp name','CoCp energy', 'Cp name','Cp energy','BDE', 'Dipole moment', 'CoCp2 HOMO energy', 'CoCp2 LUMO energy', 'CoCp HOMO energy', 'CoCp LUMO energy', 'Cp HOMO energy', 'Cp LUMO energy','CoCp H charge','Cp H charge','CoCp polarizability','Cp polarizability','Hardness','dCOM','rCo-O'])
+der_data = pd.DataFrame(columns=['Name', 'CoCp2 energy', 'CoCp name','CoCp energy', 'Cp name','Cp energy','BDE', 'Dipole moment', 'CoCp2 HOMO energy', 'CoCp2 LUMO energy', 'CoCp HOMO energy', 'CoCp LUMO energy',
+ 'Cp HOMO energy', 'Cp LUMO energy','CoCp H charge','Cp H charge','CoCp polarizability','Cp polarizability','Hardness','dCOM','rCo-O'])
 
-cocp2_der = np.loadtxt("der.dat", dtype='str')
+cocp2_der = np.loadtxt("der.dat", dtype='str') #der,dat includes the names for all di-substituted cocp2oh  
+
 
 energy=np.empty(len(cocp2_der))
 dipole=np.empty(len(cocp2_der))
@@ -39,40 +41,41 @@ lumo=np.empty(len(cocp2_der))
 r_com=np.empty(len(cocp2_der))
 r=np.empty(len(cocp2_der))
 
-out_path='../out_files/'
-xyz_path='../xyz_files'
+out_path='path2out_files'   #define the path to QChem output files
+xyz_path='path2xyz_files'   #define the paths to extracted xyz files
 
 for i in range(len(cocp2_der)):
-	file=open(os.path.join(out_path,cocp2_der[i]+'.out'),"r")
-	data=file.readlines()
-	for indx,line in enumerate(data):
-		if 'Final energy is' in line:
-			energy[i]=(line.split()[3])
-		if 'OPTIMIZATION CONVERGED' in line:
-			d[0]=indx
-		if 'Z-matrix' in line:
-			d[1]=indx
-		if '-- Virtual --' in line:
-			d[2]=indx
-		if 'Dipole Moment (Debye)' in line:
-			d[3]=indx
-	file.close()
+        file=open(os.path.join(out_path,cocp2_der[i]+'.out'),"r")
+        data=file.readlines()
+        for indx,line in enumerate(data):
+                if 'Final energy is' in line:
+                        energy[i]=(line.split()[3])
+                if 'OPTIMIZATION CONVERGED' in line:
+                        d[0]=indx
+                if 'Z-matrix' in line:
+                        d[1]=indx
+                if '-- Virtual --' in line:
+                        d[2]=indx
+                if 'Dipole Moment (Debye)' in line:
+                        d[3]=indx
+        file.close()
 
-	file=open(os.path.join(out_path,cocp2_der[i]+'.out'),"r")
-	data=file.readlines()
-	write_file=open(os.path.join(xyz_path,cocp2_der[i]+'.xyz'), "w")
-	for indx, line in enumerate(data):
-		if indx>=d[0]+5:
-			if indx<d[1]:
-				write_file.write(line)
-		if indx==d[2]-1:
-			homo[i]=(line.split()[-1])
-		if indx==d[2]+1:
-			lumo[i]=line.split()[0]
-		if indx==d[3]+2:
-			dipole[i]=(line.split()[1])
-	write_file.close()
-	file.close()
+        file=open(os.path.join(out_path,cocp2_der[i]+'.out'),"r")
+        data=file.readlines()
+        write_file=open(os.path.join(xyz_path,cocp2_der[i]+'.xyz'), "w")
+        for indx, line in enumerate(data):
+                if indx>=d[0]+5:
+                        if indx<d[1]:
+                                write_file.write(line)
+                if indx==d[2]-1:
+                        homo[i]=(line.split()[-1])
+                if indx==d[2]+1:
+                        lumo[i]=line.split()[0]
+                if indx==d[3]+2:
+                        dipole[i]=(line.split()[1])
+        write_file.close()
+        file.close()
+
 
 der_data['CoCp2 energy'] = energy[:]
 der_data['Dipole moment'] = dipole[:]
@@ -86,20 +89,27 @@ homo_file=open('homo.dat',"w")
 lumo_file=open('lumo.dat',"w")
 
 for i in range(len(cocp2_der)):
-	energy_file.write(cocp2_der[i]+ '   ' +str(energy[i])+'\n')
-	dipole_file.write(str(dipole[i])+'\n')
-	homo_file.write(str(homo[i])+'\n')
-	lumo_file.write(str(lumo[i])+'\n')
+        energy_file.write(cocp2_der[i]+ '   ' +str(energy[i])+'\n')
+        dipole_file.write(str(dipole[i])+'\n')
+        homo_file.write(str(homo[i])+'\n')
+        lumo_file.write(str(lumo[i])+'\n')
 energy_file.close()
 dipole_file.close()
 homo_file.close()
 lumo_file.close()
 
-
+#shorten names for the substituents
+#update the 'der' array with new substituent name
 der=['dme','meth','eth','pr2','thl','c6h5','c2h3','c2h','ch3','tch3','c3h7','h','cl','br','f','conhr',
      'conh2','cooch3','coch3','coh','cof','cocl','cf3','ccl3','cbr3','cn','soor','no2','soocf3','ph_p_cl',
      'ph_o_cl','ph_m_cl','ph_o_cf3','ph_m_cf3','ph_p_cf3','ph_p_ch3','ph_p_och3','ph_op_ch3','ph_op_och3',
      'piperidine','paryl2','naryl2']
+#chemical notations to the substituents
+#update the 'der_new' array with new substituent name
+der_new=[u'N(CH\u2083)\u2082',u'OCH\u2083',u'O(CO)CH\u2083',u'P(CH\u2083)\u2082',u'S(CH\u2083)',u'Ph',u'C\u2082H\u2083',u'C\u2082H',u'CH\u2083',u'C(CH\u2083)\u2083',u'C\u2083H\u2087',
+     'H','Cl','Br','F',u'(CO)NHCH\u2083',u'(CO)NH\u2082',u'(CO)OCH\u2083',u'(CO)CH\u2083','(CO)H','(CO)F','(CO)Cl',u'CF\u2083',u'CCl\u2083',u'CBr\u2083','CN',u'SOOCH\u2083',u'NO\u2082',u'SOOCF\u2083',
+    'p-PhCl','o-PhCl','m-PhCl',u'o-PhCF\u2083',u'm-PhCF\u2083',u'p-PhCF\u2083',u'p-PhCH\u2083',u'p-PhOCH\u2083',u'op-Ph(CH\u2083)\u2083',u'op-Ph(OCH\u2083)\u2083',u'NC\u2085H\u2081\u2080',u'P(Ph)\u2082',u'N(Ph)\
+u2082']
 
 bde_summary=open(os.path.join('bde_summary.dat'),'w')
 
@@ -124,18 +134,19 @@ for i in range(num):
                         cp_j_hcharge,cp_i_hcharge=der_energy(der[j],der[i],'h_charge')
                         cocp_i_pol,cocp_j_pol=der_energy(der[i],der[j],'pol')
                         cp_j_pol,cp_i_pol=der_energy(der[j],der[i],'pol')
-                        
+
                         tot_ij = cocp_i + cp_j
                         tot_ji = cocp_j + cp_i
-        
+
+                        der_data.at[k,'Name']=str('Co(Cp-'+der_new[i]+')(Cp-'+der_new[j]+')OH')
+                        #choosing the most stable orientation to dissociate the di-substituted complex into fragments 
                         if tot_ij <= tot_ji:
                                 bde= (tot_ij - cocp2_energy)*627.5
                                 bde_summary.write( cocp2_name +'>> cocp : '+der[i]+' '+'>> cp : '+der[j]+' '+ '>> BDE : '+str(bde)+'\n')
-                                
-                                der_data.at[k,'Name']=cocp2_name
-                                der_data.at[k,'CoCp name']=der[i]
+
+                                der_data.at[k,'CoCp name']=der_new[i]
                                 der_data.at[k,'CoCp energy']=cocp_i
-                                der_data.at[k,'Cp name']=der[j]
+                                der_data.at[k,'Cp name']=der_new[j]
                                 der_data.at[k,'Cp energy']=cp_j
                                 der_data.at[k,'BDE']=bde
                                 der_data.at[k,'CoCp HOMO energy']=cocp_i_homo
@@ -146,18 +157,17 @@ for i in range(num):
                                 der_data.at[k,'Cp H charge']=cp_j_hcharge
                                 der_data.at[k,'CoCp polarizability']=cocp_i_pol
                                 der_data.at[k,'Cp polarizability']=cp_j_pol
-                                
+
                         else:
                                 bde=(tot_ji-cocp2_energy)*627.5
                                 bde_summary.write(cocp2_name +'>> cocp : '+der[j]+' '+'>> cp : '+der[i]+' '+ '>> BDE : '+str(bde)+'\n')
 
 
-                                der_data.at[k,'Name']=cocp2_name
-                                der_data.at[k,'CoCp name']=der[j]
+                                der_data.at[k,'CoCp name']=der_new[j]
                                 der_data.at[k,'CoCp energy']=cocp_j
-                                der_data.at[k,'Cp name']=der[i]
+                                der_data.at[k,'Cp name']=der_new[i]
                                 der_data.at[k,'Cp energy']=cp_i
-                                der_data.at[k,'BDE']=bde                        
+                                der_data.at[k,'BDE']=bde
                                 der_data.at[k,'CoCp HOMO energy']=cocp_j_homo
                                 der_data.at[k,'CoCp LUMO energy']=cocp_j_lumo
                                 der_data.at[k,'Cp HOMO energy']=cp_i_homo
@@ -166,7 +176,7 @@ for i in range(num):
                                 der_data.at[k,'Cp H charge']=cp_i_hcharge
                                 der_data.at[k,'CoCp polarizability']=cocp_j_pol
                                 der_data.at[k,'Cp polarizability']=cp_i_pol
-
+            #calculating the rcom and rco-o distances
             opt_xyz = open(os.path.join(xyz_path,str('di_cocp2_'+der[i]+'_'+der[j]+'.xyz')),"r").readlines()
             corx1=0.0
             cory1=0.0
@@ -192,7 +202,7 @@ for i in range(num):
                       corx2= corx2 + float(line_n.split()[2])
                       cory2= cory2 + float(line_n.split()[3])
                       corz2= corz2 + float(line_n.split()[4])
-      
+
                 if n==9:
                    x_co= x_co + float(line_n.split()[2])
                    y_co= y_co + float(line_n.split()[3])
@@ -202,8 +212,9 @@ for i in range(num):
                    x_o= x_o + float(line_n.split()[2])
                    y_o= y_o + float(line_n.split()[3])
                    z_o= z_o + float(line_n.split()[4])
-
+            #the distance between center of masses of two cyclopentadienyl rings 
             r_com= math.sqrt(((corx1-corx2)/5)**2 + ((cory1-cory2)/5)**2 + ((corz1-corz2)/5)**2)
+            #the distance between Co atom and the O of OH anion
             r= math.sqrt((x_co-x_o)**2 + (y_co-y_o)**2 + (z_co-z_o)**2)
             print(str(r_com))
 
@@ -212,4 +223,4 @@ for i in range(num):
 
 
 bde_summary.close()
-der_data.to_excel("output.xlsx") 
+der_data.to_excel("cobaltocenium_data.xlsx")
